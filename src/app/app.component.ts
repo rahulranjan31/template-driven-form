d8a058004887073c4bf00149eba974b568de240e
Rahul Ranjan <ranjan.rahul326@gmail.com>
2017-04-27 11:14:03 +0530

    form (template driven)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tempelate Driven Form';
+  myName= "Rahul Ranjan";
+  onSubmit(value: any) {
+    
+    console.log('It works!');
+    console.log(value);
+
+  }
 }
